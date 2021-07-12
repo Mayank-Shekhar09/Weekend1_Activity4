c409fcbdf0f33c5852e20656376018530e2153dd
Mayank-Shekhar09 <mayank.shekhar@ltts.com>
2021-07-12 15:05:54 +0530

    Activity 4.

--- MyCart3BL/Purchase.cs
+++ MyCart3BL/Purchase.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace MyCart3BL
 {
     public class Purchase
+
     {
         private Customer customer;
         private DateTime dateOfPurchase;
@@ -29,12 +30,25 @@ namespace MyCart3BL
 
         public double CalculateBillAmount(params Product[] products)
         {
-            
+            double totalPrice = 0.0;
+            foreach (Product price in products)
+            {
+                totalPrice += Convert.ToDouble(price);
+            }
+
+            double serviceTax = 0.07 * totalPrice;
+            double discountPercentage = CalculateDiscount();
+            double totalBillAmount = ((totalPrice + serviceTax) * (1 - discountPercentage));
+            return totalBillAmount;
         }
 
         public double CalculateBillAmount(Product product, int quantityRequired)
         {
-
+            double totalPrice = product.Price * quantityRequired;
+            double serviceTax = 0.07 * totalPrice;
+            double discountPercentage = CalculateDiscount();
+            double totalBillAmount = ((totalPrice + serviceTax) * (1 - discountPercentage));
+            return totalBillAmount;
         }
         public double CalculateDiscount()
         {
